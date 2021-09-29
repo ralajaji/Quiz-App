a49eae8a39a0b2ad66148d8ac991fc43321e0f62
Mohammed Aljouei <mohammed.aljouei@gmail.com>
2021-09-29 04:36:46 +0300

    sign in + sign up

--- lib/screens/scoreboard.dart
+++ lib/screens/scoreboard.dart
@@ -1,110 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
-class Scoreboard extends StatefulWidget {
-  @override
-  State<Scoreboard> createState() => ScoreboardState();
+Future logoutTestPurpose() async {
+  final _storage = FlutterSecureStorage();
+  await _storage.deleteAll();
 }
 
-class ScoreboardState extends State<Scoreboard> {
-  List<Object> strings = [];
-
+class Scoreboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    void setList() async {
-      var url =
-          "https://quiz-ksu-default-rtdb.asia-southeast1.firebasedatabase.app/users.json";
-      final res = await http.get(Uri.parse(url));
-      final body = json.decode(res.body);
-
-      Map<String, dynamic> a = body;
-      List<Object> b = List<Object>();
-
-      print(strings.length);
-
-      a.forEach((key, val) {
-        if (val['user'] != null) {
-          b.add(val['user']);
-        }
-      });
-
-      setState(() {
-        strings = b;
-        print(strings);
-      });
-    }
-
-    void logOut() async {
-      final _storage = FlutterSecureStorage();
-      await _storage.deleteAll();
-
-      Navigator.pushNamed(context, '/auth');
-    }
-
-    return ListView(
-      padding: const EdgeInsets.all(8),
-      children: <Widget>[
-        Text(''),
-        Padding(
-          padding: EdgeInsets.only(bottom: 5, top: 25),
-          child: ButtonTheme(
-            height: 56,
-            child: RaisedButton(
-              child: Text('See Scoreboard ',
-                  style: TextStyle(color: Colors.white, fontSize: 20)),
-              color: Color.fromRGBO(255, 188, 117, 1).withOpacity(0.9),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50)),
-              onPressed: () => setList(),
-            ),
-          ),
+    return Container(
+      child: Center(
+        child: RaisedButton(
+          onPressed: () => logoutTestPurpose(),
+          child: Text('test'),
         ),
-        Text(''),
-        Column(
-            children: strings
-                .map((item) => Padding(
-                      padding: EdgeInsets.only(bottom: 5, top: 25),
-                      child: ButtonTheme(
-                        height: 56,
-                        child: RaisedButton(
-                          child: Text(
-                              item.toString().split('{')[1].split('}')[0],
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 20)),
-                          color:
-                              Color.fromRGBO(215, 117, 255, 1).withOpacity(0.5),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(50)),
-                          onPressed: () => setList(),
-                        ),
-                      ),
-                    ))
-                .toList()),
-        Text(''),
-        Padding(
-          padding: EdgeInsets.only(bottom: 5, top: 500),
-          child: ButtonTheme(
-            height: 56,
-            child: RaisedButton(
-              child: Text('Logout',
-                  style: TextStyle(color: Colors.white, fontSize: 20)),
-              color: Colors.red,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50)),
-              onPressed: () => logOut(),
-            ),
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
-
-// void logOut() async {
-//   final _storage = FlutterSecureStorage();
-//   await _storage.deleteAll();
-
-//   Navigator.pushNamed(context, '/auth');
-// }
